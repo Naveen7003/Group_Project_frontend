155534b89abf3d8029a8137e450a888700cd1900
Naveen Prajapati <prajapatinaveen7003@gmail.com>
2024-10-04 21:56:40 +0530

    localhost

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: "https://group-project-tbwn.onrender.com",
+    baseURL: "http://localhost:8000",
     withCredentials:true,
 })
 
